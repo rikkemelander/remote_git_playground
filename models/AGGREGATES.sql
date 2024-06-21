1c8a2eb7bb44825be3ecf2926c1b0cd881a92fed
Admin Jensen <admin@wayfare.ai>
2024-06-21 16:29:57 +0200

    fix

--- models/AGGREGATES.sql
+++ models/AGGREGATES.sql
@@ -1,10 +1,9 @@
 select
     pc.categoryname,
-    psc.SUBCATEGORYNAME,
     sum(p.productcost + p.productprice) as total_cost
 from {{ ref('STG_PRODUCTS') }} as p
 left join {{ ref('STG_PRODUCTSUBCATEGORIES') }} as psc
     on p.productsubcategorykey = psc.productsubcategorykey
 left join {{ ref('STG_PRODUCTCATEGORIES') }} as pc
     on psc.productcategorykey = pc.productcategorykey
-group by pc.categoryname, psc.SUBCATEGORYNAME
+group by pc.categoryname
