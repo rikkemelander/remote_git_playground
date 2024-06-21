d1c9a3712b6dd64adf933fb188e3f2c3c26eb820
Admin Jensen <admin@wayfare.ai>
2024-06-21 16:14:36 +0200

    bad

--- models/STG_PRODUCTCATEGORIES.sql
+++ models/STG_PRODUCTCATEGORIES.sql
@@ -1,5 +1,5 @@
 select
-    productcategories.categoryname || 'hello' as categoryname,
+    productcategories.categoryname as categoryname,
     productcategories.productcategorykey as productcategorykey,
     productcategories._wayfare_created_at as _wayfare_created_at,
     productcategories._wayfare_updated_at as _wayfare_updated_at
