ab2c7dd512010dcc963c1749eaccb2366ef4ddda
Admin Jensen <admin@wayfare.ai>
2024-06-21 16:13:55 +0200

    boom

--- models/AGGREGATES.sql
+++ models/AGGREGATES.sql
@@ -1,9 +1,9 @@
 select
     pc.categoryname,
     sum(p.productcost + p.productprice) as total_cost
-from {{ ref('stg_products') }} as p
-left join {{ ref('stg_productsubcategories') }} as psc
+from {{ ref('STG_PRODUCTS') }} as p
+left join {{ ref('STG_PRODUCTSUBCATEGORIES') }} as psc
     on p.productsubcategorykey = psc.productsubcategorykey
-left join {{ ref('stg_productcategories') }} as pc
+left join {{ ref('STG_PRODUCTCATEGORIES') }} as pc
     on psc.productcategorykey = pc.productcategorykey
 group by pc.categoryname
