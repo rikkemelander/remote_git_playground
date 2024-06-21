c4d401a1a19fe168d63b99acf2af0e8e865e55bd
Admin Jensen <admin@wayfare.ai>
2024-06-21 16:09:35 +0200

    impact!

--- models/stg_productcategories.sql
+++ models/stg_productcategories.sql
@@ -1,6 +1,5 @@
 select
-    productcategories.categoryname as categoryname,
-    productcategories.productcategorykey as productcategorykey,
+    productcategories.productcategorykey as categoryname,
     productcategories._wayfare_created_at as _wayfare_created_at,
     productcategories._wayfare_updated_at as _wayfare_updated_at
 from {{ source('wayfare', 'PRODUCTCATEGORIES') }} as productcategories
