464e49d43f4853f13de6a9e413be767a9e47d45d
Admin Jensen <admin@wayfare.ai>
2024-06-21 14:19:31 +0200

    test

--- models/impact-analysis-test/MODEL_2.sql
+++ models/impact-analysis-test/MODEL_2.sql
@@ -1,5 +1,5 @@
 {{ config(materialized='view') }}
 select
-    _wayfare_created_at as c,
+    _wayfare_created_at as b,
     CONCAT(dummy_col, '_test') as a
 from {{ source('wayfare', 'dummy_source_dataset') }}
