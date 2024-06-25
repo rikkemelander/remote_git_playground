77b04696f2851cc393d1040de4745ff01889ab2f
Admin Jensen <admin@wayfare.ai>
2024-06-25 17:52:49 +0200

    change things

--- models/STG_PRODUCTSUBCATEGORIES.sql
+++ models/STG_PRODUCTSUBCATEGORIES.sql
@@ -3,5 +3,5 @@ select
     productsubcategories.productsubcategorykey as productsubcategorykey,
     productsubcategories.subcategoryname as subcategoryname,
     productsubcategories._wayfare_created_at as _wayfare_created_at,
-    productsubcategories._wayfare_updated_at as _wayfare_updated_at
+    productsubcategories._wayfare_updated_at | 'TEST' as _wayfare_updated_at
 from {{ source('wayfare', 'PRODUCTSUBCATEGORIES') }} as productsubcategories
