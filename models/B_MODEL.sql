1015afdb5c91d5cfe8740e5f39634d553f68d908
Admin Jensen <admin@wayfare.ai>
2024-08-27 16:46:41 +0200

    dsaf

--- models/B_MODEL.sql
+++ models/B_MODEL.sql
@@ -1,7 +1,7 @@
 {{ config(materialized='view') }}
 select
     d.a,
-    b._wayfare_created_at
+    b.a as test
 from {{ ref('DUMMY_MODEL') }} as d
 left join {{ ref('A_BLNS_MODEL') }} as b
     on d.b._wayfare_created_at = b.a
