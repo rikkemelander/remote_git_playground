85e5414b1054ee2533c584c0ac2c4b601d6482d8
Admin Jensen <admin@wayfare.ai>
2024-08-27 16:23:57 +0200

    change

--- models/DUMMY_MODEL.sql
+++ models/DUMMY_MODEL.sql
@@ -1,5 +1,5 @@
 {{ config(materialized='view') }}
 select
-    dummy_col as a,
-    _wayfare_created_at as b
+    _wayfare_created_at as b,
+    concat(dummy_col, '0') as a
 from {{ source('wayfare', 'dummy_source_dataset') }}
