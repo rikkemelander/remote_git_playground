ee38b4e1d94a41e18db8374238e3f011309e9e0b
Admin Jensen <admin@wayfare.ai>
2024-08-27 16:30:50 +0200

    test

--- models/B_MODEL.sql
+++ models/B_MODEL.sql
@@ -1,3 +1,7 @@
 {{ config(materialized='view') }}
-select d.a
+select
+    d.a,
+    b._wayfare_created_at
 from {{ ref('DUMMY_MODEL') }} as d
+left join {{ ref('BLNS_MODEL') }} as b
+    on d.b._wayfare_created_at = b._wayfare_created_at
