08e7422d9350defc8a7ff07a044b43302552ab55
Admin Jensen <admin@wayfare.ai>
2024-08-27 16:45:47 +0200

    asdf

--- models/B_MODEL.sql
+++ models/B_MODEL.sql
@@ -3,5 +3,5 @@ select
     d.a,
     b._wayfare_created_at
 from {{ ref('DUMMY_MODEL') }} as d
-left join {{ ref('BLNS_MODEL') }} as b
-    on d.b._wayfare_created_at = b._wayfare_created_at
+left join {{ ref('A_BLNS_MODEL') }} as b
+    on d.b._wayfare_created_at = b.a
