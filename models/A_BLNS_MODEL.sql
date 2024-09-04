3d0518cdda18c98ad0465007149a17352c8c931d
Admin Jensen <admin@wayfare.ai>
2024-09-04 09:26:34 +0200

    sjald

--- models/A_BLNS_MODEL.sql
+++ models/A_BLNS_MODEL.sql
@@ -2,6 +2,6 @@
 select
     blns._wayfare_created_at as a,
     concat(blns.blns_col, 'hej') as c
-from {{ source('wayfare', 'blns_dataset') }} as blns
+from {{ source('wayfaree', 'blns_dataset') }} as blns
 left join {{ ref('DUMMY_MODEL') }} as dum
     on blns._wayfare_created_at = dum.b
