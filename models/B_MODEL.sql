7f77195d6297fd47b57cc64df77f15ec4ca6536e
Admin Jensen <admin@wayfare.ai>
2024-09-09 13:52:51 +0200

    sadf

--- models/B_MODEL.sql
+++ models/B_MODEL.sql
@@ -1,6 +1,6 @@
 {{ config(materialized='view') }}
 select
-    d.a,
+    concat(d.a, 'md') as a,
     concat(b.c, 'hej') as test
 from {{ ref('DUMMY_MODEL') }} as d
 left join {{ ref('A_BLNS_MODEL') }} as b
