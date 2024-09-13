462a01d40f2c451a7b5a0f269094f7171770f2ac
Admin Jensen <admin@wayfare.ai>
2024-09-13 16:02:20 +0200

    asfd

--- models/B_MODEL.sql
+++ models/B_MODEL.sql
@@ -1,7 +1,7 @@
 {{ config(materialized='view') }}
 select
     b.c as test,
-    concat(d.a, 'md') as a
+    d.a as a
 from {{ ref('DUMMY_MODEL') }} as d
 left join {{ ref('A_BLNS_MODEL') }} as b
     on d.b = b.a
