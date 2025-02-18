d112ecadef9dafa7bf3cb54ac954863fc6a819ff
Admin Jensen <admin@wayfare.ai>
2025-02-18 12:20:17 +0100

    sdf

--- models/new.sql
+++ models/new.sql
@@ -1,1 +1,7 @@
-select * from {{ ref('a_blns_model') }}
+select
+    test.a,
+    test.c,
+    hej.a as sjov,
+    hej.test
+from {{ ref('a_blns_model') }} as test
+left join {{ ref('b_model') }} as hej
